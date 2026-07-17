bf2dce5e8f634f05c09b97e132a193750cf3e475
Andrea01Alvarez <amherrera012@gmail.com>
2026-07-17 14:24:54 -0600

    modificacion de endpoint POST

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
     .filter(Boolean);
 
   app.enableCors({
-    origin: allowedOrigins,
+    origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
